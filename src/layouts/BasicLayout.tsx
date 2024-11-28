bd27608007737cff032c42117350088a20aab166
hajaemin <woals3527@naver.com>
2024-11-28 16:52:28 +0900

    feat: 전단지

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -93,13 +93,15 @@ function BasicLayout({children}: {children: React.ReactNode}) {
                 <OrderVoiceButton/>
 
                 <button
-                    className="flex flex-col items-center w-1/3 justify-center py-2 text-xl text-gray-600 hover:text-blue-500 focus:outline-none">
+                    className="flex flex-col items-center w-1/3 justify-center py-2 text-xl text-gray-600 hover:text-blue-500 focus:outline-none"
+                    onClick={() => navigate("/flyer/read")}
+                >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                          stroke="currentColor" className="w-10 h-10 mb-1">
                         <path strokeLinecap="round" strokeLinejoin="round"
                               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.0 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
                     </svg>
-                    장바구니
+                    전단지
                 </button>
             </div>
 
