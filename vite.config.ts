ade27295aba59d5dd6cfd4df39ae87863b6d5ddf
STra <86188374+swleeSTrain@users.noreply.github.com>
2024-12-22 12:33:50 +0900

    feat: fcm

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,13 @@ export default defineConfig({
 
   server:
       {
-        host: "0.0.0.0", // 모든 네트워크 인터페이스에서 접속 허용
-        port: 5173,      // 기본 포트 (변경 가능)
+          host: "0.0.0.0", // 모든 네트워크 인터페이스에서 접속 허용
+          port: 5173,      // 기본 포트 (변경 가능)
+          fs: {
+              allow: ['/home/ec2-user/allmart_customer']
+          }
+
+
       },
+
 })
